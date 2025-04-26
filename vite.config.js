1c3700a36a4f9300884b70014c66814f98a971f1
kollynce <collinsodeny@gmail.com>
2025-04-26 19:25:46 +0300

    feat: update base path in Vite configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/Collins-Kreation/',
   server: {
     port: 3000, // You can change this to any port number you prefer
     open: true  // This will automatically open the browser
