97d6500e09b61ac9e1e598ec3cf2fd744d9c4e2e
kollynce <collinsodeny@gmail.com>
2025-05-04 15:29:46 +0300

    feat: update Firebase hosting configuration and add initial index.html

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Collins-Kreation/',
   server: {
-    port: 3000, // You can change this to any port number you prefer
-    open: true  // This will automatically open the browser
+    port: 3000,
+    open: true
   }
 })
